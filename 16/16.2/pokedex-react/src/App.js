8be4f386c16ce0490e679a3d4fd3dea9314db4d6
Leonnardo Lair <leonnardo0316@gmail.com>
2021-02-23 16:30:36 -0300

    changes

---
+++ 16/16.2/pokedex-react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Pokedex from './Pokedex';
+
+class App extends React.Component {
+  return() {
+    <Pokedex/>
+  }
+}
+
+export default App;
